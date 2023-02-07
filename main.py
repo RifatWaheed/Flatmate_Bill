98960c670fe284d1c3edd4fd3c7616c74218b7e1
RifatWaheed <rifatwaheed@gmail.com>
2023-02-07 15:14:17 +0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+class Bill:
+    def __init__(self,amount,period):
+        self.amount = amount
+        self.period = period
+
+
+class Flatmate:
+    def __init__(self,name,days_in_house):
+        self.name = name
+        self.days_in_house = days_in_house
+
+    def pays(self,bill,flatmate2):
+        weight= self.days_in_house/(self.days_in_house + flatmate2.days_in_house)
+        to_pay = bill.amount*weight
+        return to_pay
+
+
+class PdfReport:
+    def __init__(self,filename):
+        self.filename = filename
+
+    def generate(self,flatmate1,flatmate2,bill):
+        pass
+
+the_bill = Bill(amount=120 , period= "March 2021")
+
+john = Flatmate(name='John',days_in_house=20)
+marry = Flatmate(name='Marry',days_in_house=25)
+
+print(john.pays(bill=the_bill,flatmate2=marry))
